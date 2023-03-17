2a568207afd67cebee30a57c3329dc3a430e53d9
Anderson <eusouanderson@outlook.com>
2023-03-17 09:37:20 -0300

    "Update"

--- Curso-JavaScript-Guanabara-master/modulo_d/12_Condicoes_Parte_Dois/divertimento_11.js
+++ Curso-JavaScript-Guanabara-master/modulo_d/12_Condicoes_Parte_Dois/divertimento_11.js
@@ -1,11 +1,14 @@
 // Idade para votar
 
-var idade = 67
-console.log(`Você tem ${idade} anos.`)
-if (idade < 16) {
-    console.log("Não vota.")
-} else if (idade < 18 || idade > 65) {
-        console.log("Voto opcional.")
-    } else {
-        console.log("Voto obrigatório.")
-    }
+var idade = 22
+console.log(`Você tem ${idade} anos `)
+if (idade < 16){
+    console.log('Não Vota')
+
+}else  if (idade < 18 || idade > 65){
+    console.log('Voto Opcional')
+}else {
+    console.log('Voto Obrigatório')
+}
+
+
